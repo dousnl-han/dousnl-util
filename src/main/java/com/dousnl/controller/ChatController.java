4fc0c0f8e00167f925c2e3dae7ca8cf9ec2cdfb3
DousnlHan <hanliangliang@dushu365.com>
2024-06-26 16:18:02 +0800

    cozeChatV1

--- src/main/java/com/dousnl/controller/ChatController.java
+++ src/main/java/com/dousnl/controller/ChatController.java
@@ -1,6 +1,8 @@
 package com.dousnl.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.dousnl.utils.http.HttpClentUtils;
 import com.dousnl.utils.http.Result;
 import com.google.common.collect.Maps;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
 import java.util.HashMap;
@@ -23,7 +26,7 @@ import java.util.Map;
  * Modify     : 修改日期          修改人员        修改说明          JIRA编号
  * v1.0.0       2024/6/24       hll    新增              1001
  ********************************************************************/
-@Controller
+@RestController
 @RequestMapping("/chat")
 public class ChatController {
 
@@ -33,9 +36,10 @@ public class ChatController {
 
         String question = MapUtils.getString(params, "qe");
 
-        String url = "https://api.com/v1/chat/completions";
+        String url = "https://api.openai.com/v1/chat/completions";
         final HashMap<String, String> headerMap = Maps.newHashMap();
         headerMap.put("Content-Type", "application/json");
+        headerMap.put("Authorization", "Bearer sk-wgqkRT7j3PJQ6SL4u9UANwcbSrc6D3CSDas6ZBoYhYtxuKr6");
 
         final HashMap<String, Object> paramMap = Maps.newHashMap();
         paramMap.put("model", "gpt-3.5-turbo");
@@ -70,5 +74,57 @@ public class ChatController {
         System.out.println(JSON.toJSONString(body));
         return JSON.toJSONString(body);
     }
+
+    @PostMapping("/perplexity/v1")
+    public String perplexityChatV1(@RequestBody Map<String, String> params) throws IOException {
+
+        String question = MapUtils.getString(params, "qe");
+        final String API_URL = "https://api.perplexity.ai/chat/completions";
+        final String API_KEY = "your_api_key_here";
+
+
+        final HashMap<String, String> headerMap = Maps.newHashMap();
+        headerMap.put("Content-Type", "application/json");
+        headerMap.put("Authorization", "Bearer " + API_KEY);
+
+
+        final HashMap<String, Object> paramMap = Maps.newHashMap();
+        paramMap.put("model", "mistral-7b-instruct");
+        paramMap.put("messages", "[{\"role\":\"user\",\"content\":\"+" + question +"\"}]");
+        paramMap.put("temperature", 0.7);
+        final Result post = HttpClentUtils.postJson(API_URL, headerMap, paramMap, "utf-8");
+        String body = post.getBody();
+        int statusCode = post.getStatusCode();
+        System.out.println(statusCode);
+        System.out.println(JSON.toJSONString(body));
+        return JSON.toJSONString(body);
+    }
+
+    @PostMapping("/coze/v1")
+    public String cozeChatV1(@RequestBody Map<String, String> params) throws IOException {
+
+        String question = MapUtils.getString(params, "qe");
+        final String API_URL = "https://api.coze.cn/open_api/v2/chat";
+        final String API_KEY = "pat_DlPp7h3CZFST6uoSAYGOq6wWdeb7ekRqKlKbWOABtE9XEPnchIfLGYvysgDjd1Px";
+
+
+        final HashMap<String, String> headerMap = Maps.newHashMap();
+        headerMap.put("Content-Type", "application/json");
+        headerMap.put("Authorization", "Bearer " + API_KEY);
+
+
+        final HashMap<String, Object> paramMap = Maps.newHashMap();
+        paramMap.put("bot_id", "7384676542137925641");
+        paramMap.put("user", "29032201862555");
+        paramMap.put("query", question);
+        paramMap.put("stream", false);
+        final Result post = HttpClentUtils.postJson(API_URL, headerMap, paramMap, "utf-8");
+        String body = post.getBody();
+        int statusCode = post.getStatusCode();
+        System.out.println(statusCode);
+        System.out.println(JSON.toJSONString(body));
+        return JSON.toJSONString(body);
+    }
+
 
 }
